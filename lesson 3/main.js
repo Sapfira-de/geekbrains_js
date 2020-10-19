f3a0892f66230a6b7b1fba30d3667799baeed49d
Sapfira-de <49364185+Sapfira-de@users.noreply.github.com>
2020-10-19 14:42:53 +0800

    Add files via upload

---
+++ lesson 3/main.js
@@ -0,0 +1,105 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// Переделать в ДЗ не использовать fetch а Promise
+let getRequest = (url, cb) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status !== 200) {
+        console.log('Error');
+      } else {
+        cb(xhr.responseText);
+      }
+    }
+  };
+  xhr.send();
+};
+
+// Функция запроса / ответа на промисах
+function getRequest(url, cb) {
+  return new Promise((resolve, reject) => {
+      // console.log('Работает промис');
+      let xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject;
+      xhr.open("GET", url, true);
+      xhr.onload = () => resolve(cb(xhr.responseText));
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    });
+}
+
+// –--------------------------------
+
+class ProductList {
+  #goods;
+
+  constructor(container = '.products') {
+    this.container = container;
+    this.#goods = [];
+    this._allProducts = [];
+
+    // this._fetchGoods();
+    this.#getProducts().then((data) => {
+      this.#goods = [...data];
+      // this.#goods = Array.from(data);
+      this.#render();
+    });
+
+    console.log(this.sum());
+  }
+
+  // _fetchGoods() {
+  //   getRequest(`${API}/catalogData.json`, (data) => {
+  //     console.log(data);
+  //     this.#goods = JSON.parse(data);
+  //     this.#render();
+  //     console.log(this.#goods);
+  //   });
+  // }
+
+  #getProducts() {
+    return fetch(`${API}/catalogData.json`)
+        .then(response => response.json())
+        .catch((error) => {
+          console.log(error);
+        });
+  }
+
+  sum() {
+    return this.#goods.reduce((sum, { price }) => sum + price, 0);
+  }
+
+  #render() {
+    const block = document.querySelector(this.container);
+
+    for (let product of this.#goods) {
+      const productObject = new ProductItem(product);
+
+      this._allProducts.push(productObject);
+
+      block.insertAdjacentHTML('beforeend', productObject.getGoodHTML());
+    }
+  }
+}
+
+class ProductItem {
+  constructor(product, img='https://placehold.it/200x150') {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
+
+  getGoodHTML() {
+    return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+            </div>`;
+  }
+}
+
+const list = new ProductList();
