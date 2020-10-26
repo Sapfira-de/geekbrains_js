9112c0f13c2578b1e90fdbbf5be34502ec8f2a58
Sapfira-de <49364185+Sapfira-de@users.noreply.github.com>
2020-10-26 17:18:34 +0800

    Update task_3.js

--- lesson4/task_3.js
+++ lesson4/task_3.js
@@ -4,7 +4,8 @@ function rightForm() {
         regExp_phone = /^\+\d{1,3}\s?\(\d{3}\)\s?\d{3}(-\d{2}){2}$/,
         regExp_message = /[a-zа-яё0-9]/;
         
- let name = document.getElementsByName('name')[0].value;
+ let name = document.getElementsByName('name')[0].value,
+     email = document.getElementsByName('email').value;
  
    // имя
     if(regExp_name.test(name) === true) {
@@ -13,4 +14,11 @@ function rightForm() {
         document.getElementById('name').className = 'error';
     }
  
+     // e-mail
+    if(regExp_email.test(email) === true) {
+        document.getElementById('email').className = 'ok';
+    } else {
+        document.getElementById('email').className = 'error';
+    }
+    
  document.querySelector('.button').addEventListener("click", rightForm);
