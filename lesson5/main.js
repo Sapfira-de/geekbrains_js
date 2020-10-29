0a03a4d91d69ade5d1811b0883b8c1674fdbe7d3
Sapfira-de <49364185+Sapfira-de@users.noreply.github.com>
2020-10-29 11:57:48 +0800

    Add files via upload

---
+++ lesson5/main.js
@@ -0,0 +1,52 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        imgCatalog: 'https://placehold.it/200x150',
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product) {
+            console.log(product.id_product)
+        },
+    },
+    beforeCreate() {
+        console.log('beforeCreate');
+    },
+    created() {
+        console.log('created');
+        this.getJson(`${API}${this.catalogUrl}`)
+            .then(data => {
+                for (el of data) {
+                    this.products.push(el);
+                }
+            });
+    },
+    beforeMount() {
+        console.log('beforeMount');
+    },
+    mounted() {
+        console.log('mounted');
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate');
+    },
+    updated() {
+        console.log('updated');
+    },
+    beforeDestroy() {
+        console.log('beforeDestroy');
+    },
+    destroyed() {
+        console.log('destroyed');
+    },
+});
