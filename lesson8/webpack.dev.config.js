b3286f1030b9300772ca95cc61418bac9e18e9a4
Sapfira-de <49364185+Sapfira-de@users.noreply.github.com>
2020-11-12 13:14:37 +0800

    Add files via upload

---
+++ lesson8/webpack.dev.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: ["@babel/polyfill", "./src/public/index.js"]
+    },
+    output: {
+        path: path.join(__dirname, 'dist/public'),
+        publicPath: "/",
+        filename: "js/[name].js"
+    },
+    target: 'web',
+    devtool: "#source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/public/index.html',
+            filename: 'index.html',
+            excludeChunks: ['server']
+        })
+    ]
+};
